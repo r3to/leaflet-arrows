c96a795932ac2a16834ad175170198fc85c332ec
Reto Lehnherr <reto.lehnherr@meteotest.ch>
2015-02-11 08:22:11 +0100

    convert tabs to spaces and reformat code

--- mt-leaflet-arrows.js
+++ mt-leaflet-arrows.js
@@ -2,163 +2,165 @@ var MT = MT || {};
 // arrow module -> requires Leaflet!
 // https://github.com/meteotest/leaflet-arrows
 MT.arrows = (function() {
-	/**
-	 * private functions and attributes to make code more save
-	 */
-	var config = {
-		stretchFactor : 1, // should the distance be stretched?
-		arrowheadLength : 4, // in km
-		arrowheadClosingLine : false, // should a closing third line be drawn?
-		arrowheadDegree : 140, // degree of arrowhead
-		clickableWidth : 10, // defines the width in pixels of the "phantom" path to capture click events on a line
-		validator : function(pointData) { return typeof pointData !== 'undefined'; }, // validator is a callback function that takes the data object of the current point and returns whether it is 'valid'. Invalid arrows will be drawn gray
-		colorInvalidPoint : '#777'
-	};
+  /**
+   * private functions and attributes to make code more save
+   */
+  var config = {
+    stretchFactor: 1, // should the distance be stretched?
+    arrowheadLength: 4, // in km
+    arrowheadClosingLine: false, // should a closing third line be drawn?
+    arrowheadDegree: 140, // degree of arrowhead
+    clickableWidth: 10, // defines the width in pixels of the "phantom" path to capture click events on a line
+    validator: function(pointData) {
+      return typeof pointData !== 'undefined';
+    }, // validator is a callback function that takes the data object of the current point and returns whether it is 'valid'. Invalid arrows will be drawn gray
+    colorInvalidPoint: '#777'
+  };
 
-	function calculateEndPoint(latlng, dist, degree) {
-		/*
-		 * http://www.codeguru.com/cpp/cpp/algorithms/article.php/c5115/Geographic-Distance-and-Azimuth-Calculations.htm
-		 */
-		var distance = dist * config.stretchFactor;
-		var R = 6378.137, // earth radius in kmmeters
-		d2r = L.LatLng.DEG_TO_RAD, // degree 2 radius
-		r2d = L.LatLng.RAD_TO_DEG, // 
+  function calculateEndPoint(latlng, dist, degree) {
+    /*
+     * http://www.codeguru.com/cpp/cpp/algorithms/article.php/c5115/Geographic-Distance-and-Azimuth-Calculations.htm
+     */
+    var distance = dist * config.stretchFactor;
+    var R = 6378.137, // earth radius in kmmeters
+      d2r = L.LatLng.DEG_TO_RAD, // degree 2 radius
+      r2d = L.LatLng.RAD_TO_DEG, //
 
-		bearing = degree * d2r;
-		distance = distance / R;
-		var a = Math.acos(Math.cos(distance) *
-				Math.cos((90 - latlng.lat) * d2r) +
-				Math.sin((90 - latlng.lat) * d2r) *
-				Math.sin(distance) *
-				Math.cos(bearing));
-		var B = Math.asin(Math.sin(distance) * Math.sin(bearing) / Math.sin(a));
-		return new L.LatLng(90 - a * r2d, B * r2d + latlng.lng);
-	}
+      bearing = degree * d2r;
+    distance = distance / R;
+    var a = Math.acos(Math.cos(distance) *
+      Math.cos((90 - latlng.lat) * d2r) +
+      Math.sin((90 - latlng.lat) * d2r) *
+      Math.sin(distance) *
+      Math.cos(bearing));
+    var B = Math.asin(Math.sin(distance) * Math.sin(bearing) / Math.sin(a));
+    return new L.LatLng(90 - a * r2d, B * r2d + latlng.lng);
+  }
 
-	function calculateArrowArray(latlng, degree) {
-		// calculates the Array for the arrow
+  function calculateArrowArray(latlng, degree) {
+    // calculates the Array for the arrow
 
-		// latlng is the position, where the arrow is added
-		// degree is the degree of the line
+    // latlng is the position, where the arrow is added
+    // degree is the degree of the line
 
-		if (latlng.length !== undefined) {
-			latlng = new L.LatLng(latlng);
-		}
-		var firstEdge = calculateEndPoint(latlng, config.arrowheadLength, degree -
-				config.arrowheadDegree);
-		var arr = [ firstEdge, latlng,
-				calculateEndPoint(latlng, config.arrowheadLength, degree + config.arrowheadDegree) ];
+    if (latlng.length !== undefined) {
+      latlng = new L.LatLng(latlng);
+    }
+    var firstEdge = calculateEndPoint(latlng, config.arrowheadLength, degree -
+      config.arrowheadDegree);
+    var arr = [firstEdge, latlng,
+      calculateEndPoint(latlng, config.arrowheadLength, degree + config.arrowheadDegree)
+    ];
 
-		if (config.arrowheadClosingLine) {
-			arr.push(firstEdge);
-		}
-		return arr;
-	}
+    if (config.arrowheadClosingLine) {
+      arr.push(firstEdge);
+    }
+    return arr;
+  }
 
-	/**
-	 * public properties
-	 */
-	return {
-		/**
-		 * Takes an object as parameter and sets the configuration accordingly.
-		 * Preserves keys in the base configruation, that aren't assigned
-		 */
-		setConfiguration : function(customConfig) {
-			if (customConfig !== undefined) {
-				for ( var id in customConfig) {
-					if (customConfig.hasOwnProperty(id)) {
-						config[id] = customConfig[id];
-					}
-				}
-			}
-		},
+  /**
+   * public properties
+   */
+  return {
+    /**
+     * Takes an object as parameter and sets the configuration accordingly.
+     * Preserves keys in the base configruation, that aren't assigned
+     */
+    setConfiguration: function(customConfig) {
+      if (customConfig !== undefined) {
+        for (var id in customConfig) {
+          if (customConfig.hasOwnProperty(id)) {
+            config[id] = customConfig[id];
+          }
+        }
+      }
+    },
 
-		getStretchFactor : function() {
-			return config.stretchFactor;
-		},
+    getStretchFactor: function() {
+      return config.stretchFactor;
+    },
 
-		/**
-		 * data: object containing information for arrows 
-		 * { key : {
-		 *  	nameOfDegreeProperty* : Number (degree) 
-		 *  	nameOfDistanceProperty* :Number (km) 
-		 *  	[[nameOfColorProperty* : Any -> will be applied to the 'colorScheme' function]] opt 
-		 *		} 
-		 *  }
-		 * 
-		 * options : object for customizing path and data handling 
-		 * 	**important**  define the names of the attributes in data map, which hold the information on length, degree and the parameter for the colorScheme
-		 * function (*) 
-		 * {
-		 *	nameOfDegreeProperty : String eg. 'deg' -> name of Property in data objects containing the degree value
-		 *  nameOfDistanceProperty : String 'length'-> name of Property in data objects containing the distance value 
-		 *  nameOfColorProperty : String 'value' -> name of Property in data objects containing the value, which should be supplied to the colorScheme function 
-		 *  isWindDegree : boolean (is Degree value direction of wind? -> degree - 180 
-		 *  
-		 *  }
-		 */
-		makeArrowLayer : function(data, options, colorScheme) {
-			// options: nameOfLayer, isWindDegree, nameOfDegreeAttribute,
-			// nameOfDistanceAttribute, pathOptions, popupContent
-			
-			this.setConfiguration(options);
-						
-			var pointPathOption = {
-				stroke : false,
-				fillOpacity: 0.8,
-			};
-			
-			var allArrows = [];
-			for ( var dataId in data) {
-				var entity = data[dataId];
-				if (entity[options.nameOfDegreeProperty] !== undefined &&
-						entity[options.nameOfDistanceProperty] !== undefined) {
-					var startPoint = new L.LatLng(entity.lat, entity.lon);
-					// TODO make name of lat/long property customizable
+    /**
+     * data: object containing information for arrows
+     * { key : {
+     *    nameOfDegreeProperty* : Number (degree)
+     *    nameOfDistanceProperty* :Number (km)
+     *    [[nameOfColorProperty* : Any -> will be applied to the 'colorScheme' function]] opt
+     *    }
+     *  }
+     *
+     * options : object for customizing path and data handling
+     *  **important**  define the names of the attributes in data map, which hold the information on length, degree and the parameter for the colorScheme
+     * function (*)
+     * {
+     *  nameOfDegreeProperty : String eg. 'deg' -> name of Property in data objects containing the degree value
+     *  nameOfDistanceProperty : String 'length'-> name of Property in data objects containing the distance value
+     *  nameOfColorProperty : String 'value' -> name of Property in data objects containing the value, which should be supplied to the colorScheme function
+     *  isWindDegree : boolean (is Degree value direction of wind? -> degree - 180
+     *
+     *  }
+     */
+    makeArrowLayer: function(data, options, colorScheme) {
+      // options: nameOfLayer, isWindDegree, nameOfDegreeAttribute,
+      // nameOfDistanceAttribute, pathOptions, popupContent
 
-					var degree = options.isWindDegree ? entity[options.nameOfDegreeProperty] - 180
-							: entity[options.nameOfDegreeProperty];
-					var distance = entity[options.nameOfDistanceProperty];
-					var pathOption = options.pathOptions;
-					
-					// is current arrow valid according to the validator callback? change color if not
-					if (typeof config.validator !== 'function' || config.validator(entity)) {
-						pathOption.color = typeof colorScheme === "function" ? colorScheme(entity[options.nameOfColorProperty]) : options.color;						
-					} else {
-						pathOption.color = config.colorInvalidPoint;
-					}
+      this.setConfiguration(options);
 
-					distance = parseFloat(distance);
-					 
-					// if distance is 0 draw a point instead of an arrow
-					if (distance === 0 || distance === "undefined") {
-						pointPathOption.color = pathOption.color;
-						var circle = L.circle(startPoint, 1000, pointPathOption);
-						circle.bindPopup(options.popupContent(entity, dataId));
-						allArrows.push(circle);
-					} else {
-						var theLine = [ startPoint, calculateEndPoint(startPoint, distance, degree) ];
-						var theArrow = calculateArrowArray(theLine[1], degree);
+      var pointPathOption = {
+        stroke: false,
+        fillOpacity: 0.8,
+      };
 
-						var backgroundPathOption = $.extend(true, {}, pathOption);
-						backgroundPathOption.opacity = 0;
-						backgroundPathOption.weight = config.clickableWidth;
+      var allArrows = [];
+      for (var dataId in data) {
+        var entity = data[dataId];
+        if (entity[options.nameOfDegreeProperty] !== undefined &&
+          entity[options.nameOfDistanceProperty] !== undefined) {
+          var startPoint = new L.LatLng(entity.lat, entity.lon);
+          // TODO make name of lat/long property customizable
 
-						var backgroundMulitpolyline = new L.MultiPolyline([ theLine, theArrow ],
-								backgroundPathOption);
-						var multipolyline = new L.MultiPolyline([ theLine, theArrow ], pathOption);
+          var degree = options.isWindDegree ? entity[options.nameOfDegreeProperty] - 180 : entity[options.nameOfDegreeProperty];
+          var distance = entity[options.nameOfDistanceProperty];
+          var pathOption = options.pathOptions;
 
-						backgroundMulitpolyline.bindPopup(options.popupContent(entity, dataId));
-						allArrows.push(multipolyline);
-						allArrows.push(backgroundMulitpolyline);
-					}
-				}
-			}
-			var lg = L.layerGroup(allArrows);
-			return {
-				layerGroup : lg,
-				layerName : options.nameOfLayer
-			};
-		}
-	};
+          // is current arrow valid according to the validator callback? change color if not
+          if (typeof config.validator !== 'function' || config.validator(entity)) {
+            pathOption.color = typeof colorScheme === "function" ? colorScheme(entity[options.nameOfColorProperty]) : options.color;
+          } else {
+            pathOption.color = config.colorInvalidPoint;
+          }
+
+          distance = parseFloat(distance);
+
+          // if distance is 0 draw a point instead of an arrow
+          if (distance === 0 || distance === "undefined") {
+            pointPathOption.color = pathOption.color;
+            var circle = L.circle(startPoint, 1000, pointPathOption);
+            circle.bindPopup(options.popupContent(entity, dataId));
+            allArrows.push(circle);
+          } else {
+            var theLine = [startPoint, calculateEndPoint(startPoint, distance, degree)];
+            var theArrow = calculateArrowArray(theLine[1], degree);
+
+            var backgroundPathOption = $.extend(true, {}, pathOption);
+            backgroundPathOption.opacity = 0;
+            backgroundPathOption.weight = config.clickableWidth;
+
+            var backgroundMulitpolyline = new L.MultiPolyline([theLine, theArrow],
+              backgroundPathOption);
+            var multipolyline = new L.MultiPolyline([theLine, theArrow], pathOption);
+
+            backgroundMulitpolyline.bindPopup(options.popupContent(entity, dataId));
+            allArrows.push(multipolyline);
+            allArrows.push(backgroundMulitpolyline);
+          }
+        }
+      }
+      var lg = L.layerGroup(allArrows);
+      return {
+        layerGroup: lg,
+        layerName: options.nameOfLayer
+      };
+    }
+  };
 })();
